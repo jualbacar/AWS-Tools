7a65ca80bbf686242bf850b6f7699f38be598622
Juan Albacar <jualbacar@MBP-de-Juan.lan>
2019-12-02 17:19:19 +0100

    Add new tool for create backups before terminating instances

---
+++ AMI-creator/ami-creator.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Dump instances' config to be retired
+aws ec2 describe-instances --filters "Name=tag:Description,Values=Retiring" > instances_retiring.json
+
+# Get the instances with tag 'Description = Retiring'
+instances=$(aws ec2 describe-instances --filters "Name=tag:Description,Values=Retiring" | jq -r .Reservations[].Instances[].InstanceId)
+
+for instance in $instances
+do  
+    # Get instances' tag Name
+    instance_name=$(aws ec2 describe-instances --instance-id $instance --query 'Reservations[].Instances[].Tags[?Key == `Name`].Value' |jq .[][] -r)
+    # Create an AMI with Name = instance_id + instance_tag_name
+    ami_id=$(aws ec2 create-image --instance-id $instance --name "$instance_name - $instance" | jq .[] -r)
+    echo "CREATED AMI for --> $instance: $instance_name with ami ID $ami_id"
+done
+
+
---
+++ AMI-creator/terminate_instaces.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Get the instances with tag 'Description = Retiring'
+instances=$(aws ec2 describe-instances --filters "Name=tag:Description,Values=Retiring" | jq -r .Reservations[].Instances[].InstanceId)
+
+for instance in $instances
+do  
+    # Terminate
+    echo "Terminating instance: $instance"
+    #aws ec2 terminate-instances --instance-ids $instance
+done
