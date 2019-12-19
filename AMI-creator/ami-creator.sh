893eacb92082220c1aa153ccfd0350c5ee6b6ca3
Juan Albacar <jualbacar@MacBook-Pro-de-Juan.local>
2019-12-19 17:54:13 +0100

    Added some commented commands to filter instances by Name

--- AMI-creator/ami-creator.sh
+++ AMI-creator/ami-creator.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Dump instances' config to be retired
-aws ec2 describe-instances --filters "Name=tag:Description,Values=Retiring" > instances_retiring.json
+aws ec2 describe-instances --filters "Name=tag:Description,Values=Retiring" > instances_retiring_4.json
+
+# Print Instances' Name to be retired - two ways
+# aws ec2 describe-instances --filters "Name=tag:Description,Values=Retiring" | jq -r .Reservations[].Instances[].Tags[] | jq -r 'if .Key == "Name" then .Value else null end'| grep -v null
+# aws ec2 describe-instances --filters "Name=tag:Description,Values=Retiring" --query 'Reservations[].Instances[].Tags[?Key == `Name`].Value' --output=text
 
 # Get the instances with tag 'Description = Retiring'
 instances=$(aws ec2 describe-instances --filters "Name=tag:Description,Values=Retiring" | jq -r .Reservations[].Instances[].InstanceId)
@@ -14,5 +18,3 @@ do
     ami_id=$(aws ec2 create-image --instance-id $instance --name "$instance_name - $instance" | jq .[] -r)
     echo "CREATED AMI $ami_id for --> $instance: $instance_name - $(date "+%Y-%m-%d% - %H:%M:%S")"
 done
-
-
